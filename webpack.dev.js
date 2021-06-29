e09f5a6b45bb0f83868ce61cd65af5b06a75bbef
Anes-Marzooki <marzoukianas@gmail.com>
2021-06-29 20:19:24 +0100

    adds_babel_loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,4 +7,18 @@ module.exports = {
 		filename: 'bundle.js',
 	},
 	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
 };
