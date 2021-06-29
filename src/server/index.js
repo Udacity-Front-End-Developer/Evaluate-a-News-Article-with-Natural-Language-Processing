1639b21c49c8da1f8765acdb0eea1296c068a204
Anes-Marzooki <marzoukianas@gmail.com>
2021-06-29 20:10:55 +0100

    init:project_setup

---
+++ src/server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const _port = 3000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve('src/client/views/index.html'));
+});
+app.listen(_port, () => console.log(`Server is live at port: ${_port}`));
