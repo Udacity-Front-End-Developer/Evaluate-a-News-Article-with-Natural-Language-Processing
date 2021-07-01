42c8c87c40a5d60494a623d445c9f4a170bfe70c
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-01 16:56:32 +0100

    Add webpack plugins&loaders

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,9 @@ const path = require('path');
 const _port = 3000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve('dist/index.html'));
-	// res.sendFile(path.resolve('src/client/views/index.html'));
 });
 app.listen(_port, () => console.log(`Server is live at port: ${_port}`));
