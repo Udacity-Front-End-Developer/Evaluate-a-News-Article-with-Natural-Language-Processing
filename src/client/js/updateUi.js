a9aa0a85a6f25bb107a0ec3d9836af5318d14b80
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-07 22:00:20 +0100

    Fix displayError call inside updateUi function

--- src/client/js/updateUi.js
+++ src/client/js/updateUi.js
@@ -2,12 +2,13 @@ import { displayError } from '..';
 
 const updateUi = (data) => {
 	// If container has some data in it, empty it.
+	let resultContainer = document.querySelector('.results__data');
 	if (resultContainer.hasChildNodes()) {
 		resultContainer.innerHTML = '';
 	}
-	// Check for the status of the api's response
+	// Check for the status of the api's response.
 	if (data.status.code > 200) {
-		return displayError(data.status.msg);
+		return Client.displayError(data.status.msg);
 	}
 	// Extract the desired data from the main object.
 	const desiredData = (({ agreement, subjectivity, confidence, irony }) => ({
@@ -16,7 +17,6 @@ const updateUi = (data) => {
 		confidence,
 		irony,
 	}))(data);
-	let resultContainer = document.querySelector('.results__data');
 	let fragment = document.createDocumentFragment();
 	const dataEntries = Object.entries(desiredData);
 	for (const [entry, state] of dataEntries) {
