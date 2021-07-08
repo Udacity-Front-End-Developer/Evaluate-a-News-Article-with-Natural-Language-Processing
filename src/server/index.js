da9b276c5a009d614ae6100973a528571c9fecc7
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-08 23:32:27 +0100

    Make an index file that listens for app

--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const app = require('./app');
 const _port = 3000;
-let _dataObj = {};
 
-const app = express();
-app.use(cors());
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-	res.sendFile(path.resolve('dist/index.html'));
-});
-
-app.post('/analyze', (req, res) => {
-	(async () => {
-		_dataObj = await require('./apiCall')(process.env.API_KEY, req.body.link);
-		res.send(_dataObj);
-	})();
-});
-
-app.listen(_port, () => console.log(`Server is live at port: ${_port}.`));
+app.listen(_port, () => console.log(`Listening at http://localhost:${_port}.`));
