7dc7ebd953667bb8bd5bf88b188dbd939c904ac4
Anes-Marzooki <marzoukianas@gmail.com>
2021-07-09 21:50:49 +0100

    Add Workbox-Plugin and its supporting constructor in dev environment

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
 	entry: './src/client/index.js',
@@ -46,5 +47,6 @@ module.exports = {
 			cleanStaleWebpackAssets: false,
 			protectWebpackAssets: false,
 		}),
+		new WorkboxPlugin.GenerateSW(),
 	],
 };
